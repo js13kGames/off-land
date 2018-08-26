031f4845f00b5fd1a9b5fb7ca0e0bfab9475781f
Valeria Khusnulina <vkhusnulina@gmail.com>
2018-08-26 18:19:02 +0100

    background routines

---
+++ Background.js
@@ -0,0 +1,33 @@
+var starList = [];
+function star(x, y, r, hue, sat) {
+	this.x = x;
+	this.y = y;
+	this.r = r;
+	this.xCoef = this.r / 10;
+	this.yCoef = this.r / 2;
+	this.hue = hue;
+	this.sat = sat;
+	var getNewPosition = function(axis, coef, maxV){
+		var newPos;
+		newPos = (axis >= 0)? axis += coef : maxV;
+		newPos = (axis > maxV)? 0 : newPos;
+		return newPos;
+	};
+	this.move = function () {
+		this.x = getNewPosition(this.x, this.xCoef, 800);
+		this.y = getNewPosition(this.y, this.yCoef, 600);
+    };
+};
+
+function generateStars(){
+	starsQty = 500;
+	colorrange = [0,60,240];
+	for (var i = 0; i < starsQty; i++) {
+		var x = Math.random() * canvas.offsetWidth;
+		y = Math.random() * canvas.offsetHeight;
+		radius = Math.random() * 1.2;
+		hue = colorrange[getRandom(0,colorrange.length - 1)],
+		sat = getRandom(50,100);
+		starList.push(new star(x, y, radius, hue, sat));
+	}	
+}
