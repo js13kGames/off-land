09b748255baf987b65f7103deb568dac434315d0
vkhusnulina <vkhusnulina@gmail.com>
2018-09-07 22:48:14 +0100

    Fullscreen functionality

--- main.js
+++ main.js
@@ -34,3 +34,14 @@ window.onload = function () {
     drawCanvasGame();
 };
 
+function openFullscreen() {
+    if (canvas.requestFullscreen) {
+        canvas.requestFullscreen();
+    } else if (canvas.mozRequestFullScreen) { /* Firefox */
+        canvas.mozRequestFullScreen();
+    } else if (canvas.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        canvas.webkitRequestFullscreen();
+    } else if (canvas.msRequestFullscreen) { /* IE/Edge */
+        canvas.msRequestFullscreen();
+    }
+}
