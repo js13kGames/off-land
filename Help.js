dec4dcb7f7358060d69dbb67cd58e90a90a86929
Fernando <fer.walln@gmail.com>
2018-09-08 20:40:14 +0100

    updated mission in help

--- Help.js
+++ Help.js
@@ -9,8 +9,7 @@ function Help() {
       "Mr. Hans is lost in the vast Space and completely offline", posH,
       verticalPosition(0));
   this.mission = new TextUI(
-      "Collect all the replacements to repair the transmitter and find the way back home ", posH
-      / 8,
+      "Collect all the radio transmitter parts and find the way back home ", posH - 50,
       verticalPosition(1));
   this.collect = new TextUI("Pick up all replacements to move to next level",
       posH,
